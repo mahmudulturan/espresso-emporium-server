be5c8305296c3248ed1d6da17a101cf4f483ad4e
Mahmudul Hasan <mahmudulhasanturan@gmail.com>
2023-10-17 18:23:03 +0600

    add get method to get single data from db

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -31,6 +31,13 @@ async function run() {
         const cursor = coffeesCollection.find();
         const result = await cursor.toArray()
         res.send(result);
+    })
+
+    app.get('/coffees/:id', async(req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id)}
+        const result = await coffeesCollection.findOne(query)
+        res.send(result)
     })
 
     app.post('/coffees', async(req, res) => {
